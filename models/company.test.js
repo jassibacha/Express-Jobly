52173d343ea51958ea3c44dab15a2f65bc60f7d7
Jassi Bacha <klikster@gmail.com>
2023-09-19 16:49:39 -0700

    Added jobs to GET /company/handle, reworked tests

--- models/company.test.js
+++ models/company.test.js
@@ -205,6 +205,14 @@ describe('get', function () {
             description: 'Desc1',
             numEmployees: 1,
             logoUrl: 'http://c1.img',
+            jobs: [
+                {
+                    id: 1,
+                    title: 'J1',
+                    salary: 1000,
+                    equity: '0.0',
+                },
+            ],
         });
     });
 
